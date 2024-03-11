f1558b89ea4b3b38c099bebd3ef7f8317a42ba81
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-03-11 22:09:03 +0100

    Refactor data initialization in BunWindowsPercentageChart

--- src/app/BunWindowsTestPercentageChart.tsx
+++ src/app/BunWindowsTestPercentageChart.tsx
@@ -14,64 +14,6 @@ type Series = {
 	data: BunWindowsPercentage[];
 };
 
-const data: Series[] = [
-	{
-		label: "Bun for Windows",
-		data: [
-			{
-				date: new Date("2024-01-10"),
-				percentage: 33,
-				tweetId: "1745029301588914279",
-			},
-			{
-				date: new Date("2024-01-13"),
-				percentage: 41.98,
-				tweetId: "1745991798789693711",
-			},
-			{
-				date: new Date("2024-01-23"),
-				percentage: 56.69,
-				tweetId: "1749657420526420086",
-			},
-			{
-				date: new Date("2024-01-24"),
-				percentage: 66.18,
-				tweetId: "1750191360660107330",
-			},
-			{
-				date: new Date("2024-01-27"),
-				percentage: 69.42,
-				tweetId: "1751223179560071340",
-			},
-			{
-				date: new Date("2024-02-01"),
-				percentage: 75,
-				tweetId: "1753042191185007095",
-			},
-			{
-				date: new Date("2024-02-07"),
-				percentage: 80.24,
-				tweetId: "1755009844007072106",
-			},
-			{
-				date: new Date("2024-02-08"),
-				percentage: 83.17,
-				tweetId: "1755390539183304741",
-			},
-			{
-				date: new Date("2024-03-02"),
-				percentage: 89.11,
-				tweetId: "1763949887811735809",
-			},
-			{
-				date: new Date("2024-03-11"),
-				percentage: 91,
-				tweetId: "1767211079405375625",
-			},
-		],
-	},
-];
-
 export function BunWindowsPercentageChart({
 	initialHeight,
 	initialWidth,
@@ -79,6 +21,67 @@ export function BunWindowsPercentageChart({
 	initialHeight?: number;
 	initialWidth?: number;
 }) {
+	const data = useMemo(
+		(): Series[] => [
+			{
+				label: "Bun for Windows",
+				data: [
+					{
+						date: new Date("2024-01-10"),
+						percentage: 33,
+						tweetId: "1745029301588914279",
+					},
+					{
+						date: new Date("2024-01-13"),
+						percentage: 41.98,
+						tweetId: "1745991798789693711",
+					},
+					{
+						date: new Date("2024-01-23"),
+						percentage: 56.69,
+						tweetId: "1749657420526420086",
+					},
+					{
+						date: new Date("2024-01-24"),
+						percentage: 66.18,
+						tweetId: "1750191360660107330",
+					},
+					{
+						date: new Date("2024-01-27"),
+						percentage: 69.42,
+						tweetId: "1751223179560071340",
+					},
+					{
+						date: new Date("2024-02-01"),
+						percentage: 75,
+						tweetId: "1753042191185007095",
+					},
+					{
+						date: new Date("2024-02-07"),
+						percentage: 80.24,
+						tweetId: "1755009844007072106",
+					},
+					{
+						date: new Date("2024-02-08"),
+						percentage: 83.17,
+						tweetId: "1755390539183304741",
+					},
+					{
+						date: new Date("2024-03-02"),
+						percentage: 89.11,
+						tweetId: "1763949887811735809",
+					},
+					{
+						date: new Date("2024-03-11"),
+						percentage: 91,
+						tweetId: "1767211079405375625",
+					},
+				],
+			},
+		],
+		[]
+	);
+
 	const primaryAxis = useMemo(
 		(): AxisOptions<BunWindowsPercentage> => ({
 			getValue: (datum: BunWindowsPercentage) => datum.date,
