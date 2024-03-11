1b1f366e9779e48fb374c6fe9286f15ecadc4eb1
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-03-11 21:58:41 +0100

    Add metadataBase URL based on environment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,19 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { env } from "@/env.mjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Bun Windows test %",
 	description: "How many percentage of the tests pass for Bun for Windows?",
+	metadataBase:
+		env.VERCEL_ENV === "production"
+			? new URL("https://www.isbunwindowsyet.com")
+			: env.VERCEL_ENV === "development"
+				? new URL("http://localhost:3000")
+				: undefined,
 };
 
 export default function RootLayout({
