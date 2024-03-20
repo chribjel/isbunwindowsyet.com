c1eb04048bccabb1eba2d10f905a38b746770de3
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-03-20 10:52:12 +0100

    styiling adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import Link from "next/link";
 import { env } from "@/env.mjs";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,8 +26,34 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body className={cn(inter.className, "flex h-svh w-full flex-col")}>
 				{children}
+				<footer className="flex w-full flex-col items-center justify-center gap-2 p-8 text-sm text-gray-600 sm:flex-row sm:gap-8">
+					<Link
+						className="font-bold hover:text-purple-600"
+						href="https://x.com/chribjel"
+						target="_blank"
+					>
+						Follow me on X (chribjel)
+					</Link>
+					<p className="font-bold">
+						Created by{" "}
+						<Link
+							className="font-bold hover:text-purple-600"
+							href="https://www.bjel.ke"
+							target="_blank"
+						>
+							Christoffer Bjelke
+						</Link>
+					</p>
+					<Link
+						className="font-bold hover:text-purple-600"
+						href="https://github.com/chribjel/isbunwindowsyet.com"
+						target="_blank"
+					>
+						View source on GitHub
+					</Link>
+				</footer>
 				<Analytics />
 			</body>
 		</html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,15 @@
-import Link from "next/link";
 import { BunWindowsPercentageChart } from "./BunWindowsTestPercentageChart";
 import { data } from "./data";
 
 export default function Home() {
 	return (
-		<main className="flex h-screen w-screen flex-col items-center justify-center">
-			<h1 className="py-4 text-3xl">Is Bun Windows Yet?</h1>
-			<div className="h-full w-full">
-				<BunWindowsPercentageChart data={data} />
-			</div>
-			<footer className="flex w-full justify-between p-8 text-sm text-gray-600">
-				<Link
-					className="font-bold hover:text-purple-600"
-					href="https://x.com/chribjel"
-					target="_blank"
-				>
-					Follow me on X (chribjel)
-				</Link>
-				<p className="font-bold">
-					Created by{" "}
-					<Link
-						className="font-bold hover:text-purple-600"
-						href="https://www.bjel.ke"
-						target="_blank"
-					>
-						Christoffer Bjelke
-					</Link>
-				</p>
-				<Link
-					className="font-bold hover:text-purple-600"
-					href="https://github.com/chribjel/isbunwindowsyet.com"
-					target="_blank"
-				>
-					View source on GitHub
-				</Link>
-			</footer>
-		</main>
+		<>
+			<main className="flex h-full w-full flex-col items-center justify-center">
+				<h1 className="py-4 text-3xl">Is Bun Windows Yet?</h1>
+				<div className="flex h-full w-full items-center justify-center">
+					<BunWindowsPercentageChart data={data} />
+				</div>
+			</main>
+		</>
 	);
 }
